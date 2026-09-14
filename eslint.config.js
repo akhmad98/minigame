62269c63cfdeb7c4b6982e2aa7eadda01ee381d5
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-09-14 15:36:30 +0500

    fix: ts version lowering

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,js}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.browser,
+    },
+    rules: {},
+  },
+  eslintConfigPrettier
+);
