6ad4a3728028d4b081393945ede652a5c912827d
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-09-18 21:34:58 +0500

    fix: configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules'] },
+  eslintPluginUnicorn.configs['flat/recommended'],
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,js}'],
@@ -12,7 +14,12 @@ export default tseslint.config(
       ecmaVersion: 2022,
       globals: globals.browser,
     },
-    rules: {},
+    linterOptions: {
+        noInlineConfig: true,
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'error',
+    },
   },
   eslintConfigPrettier
 );
