bbb5966ac3f8f37fd8a46721f508bd55495dfba3
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-09-20 21:14:17 +0500

    fix: issue partly

--- src/constants/shared.ts
+++ src/constants/shared.ts
@@ -1,6 +1,6 @@
 export const AUTH_TOKEN = 'authToken';
-export const JSON_URL = '../../assets/data/game-seed.json';
-export const JSON_LEADER_URL = '../../assets/data/leaderboard.json';
+export const JSON_URL = '/assets/data/game-seed.json';
+export const JSON_LEADER_URL = '/assets/data/leaderboard.json';
 export const DESKTOP_WIN = '(max-width: 1920px)';
 export const TABLE_WIN = '(max-width: 768px)';
 export const DESKTOP_MIN_WIN = 1920;
