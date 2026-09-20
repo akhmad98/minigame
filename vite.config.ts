a54bfb96ec69db69cc34f77a655b041c9031836c
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-09-20 21:40:39 +0500

    fix: tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       'src': fileURLToPath(new URL('src', import.meta.url)),
-      '@': fileURLToPath(new URL('./', import.meta.url))
+      '@': fileURLToPath(new URL('src', import.meta.url))
     }
   }
 });
