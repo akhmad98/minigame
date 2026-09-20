772b47f0b18cf58f4aaf1638344762d9d3eaf253
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-09-20 20:56:15 +0500

    fix: typescript issue

--- components/dialogs/auth-check.ts
+++ components/dialogs/auth-check.ts
@@ -2,15 +2,15 @@ import { authService } from 'src/auth/auth-service';
 
 
 export function checkAuthActions(): void {
-    const guestActs = document.querySelector('#guest_acts');
-    const authActs = document.querySelector('#auth_acts');
+    const guestActs: HTMLElement = document.querySelector('#guest_acts') as HTMLElement;
+    const authActs: HTMLElement = document.querySelector('#auth_acts') as HTMLElement;
     
     if (!guestActs || !authActs) {
         console.error('Navbar elements missing from DOM');
         return;
     }
 
-    const lougoutButton = document.querySelector('.btn_logout');  
+    const lougoutButton: HTMLElement = document.querySelector('.btn_logout') as HTMLElement;  
 
     if (authService.checkAuth()) {
         guestActs.style.display = 'none';
